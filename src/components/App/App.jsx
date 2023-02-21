388f6c9ad1d61fa621c48dfd4dbdcb6495241fd0
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 22:43:36 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,9 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { routes } from 'helpers/routes';
 
 import { Layout } from 'layout/Layout/Layout';
-import { HomePage } from 'components/Home/Home';
-import { MoviesPage } from 'components/Movies/Movies';
-import { MovieDetailsPage } from 'components/MovieDetails/MovieDetails';
+import { HomePage } from 'pages/Home/Home';
+import { MoviesPage } from 'pages/Movies/Movies';
+import { MovieDetailsPage } from 'pages/MovieDetails/MovieDetails';
 import { Cast } from 'components/Cast/Cast';
 import { Review } from 'components/Reviews/Reviews';
 
