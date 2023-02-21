2417fd41c026520099cf63ffb17f6ff870af3571
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 02:08:25 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,41 +1,25 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { getTrendingFilms } from 'services/api';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { routes } from 'helpers/routes';
+
+import { Layout } from 'layout/Layout/Layout';
+import { HomePage } from 'components/Home/Home';
+import { MoviesPage } from 'components/Movies/Movies';
+import { MovieDetailsPage } from 'components/MovieDetails/MovieDetails';
+import { Cast } from 'components/Cast/Cast';
+import { Review } from 'components/Reviews/Reviews';
 
 export const App = () => {
-  const [trendingMovies, setTrendingMovies] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    // if (!trendingMovies) return;
-    async function getFilms() {
-      try {
-        setIsLoading(true);
-
-        const { results } = await getTrendingFilms();
-        console.log(results);
-        setTrendingMovies(results);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    getFilms();
-  }, []);
-
   return (
-    <>
-      {error && <div>Try to reload the page</div>}
-      {isLoading && <div>Loading</div>}
-      <ul>
-        {trendingMovies.map(({ id, title }) => (
-          <li key={id}>
-            <h3>{title}</h3>
-          </li>
-        ))}
-      </ul>
-    </>
+    <Routes>
+      <Route path={routes.HOME} element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path={routes.MOVIES} element={<MoviesPage />} />
+        <Route path={routes.MOVIE_DETAILS} element={<MovieDetailsPage />}>
+          <Route path={routes.CAST} element={<Cast />} />
+          <Route path={routes.REVIEWS} element={<Review />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<Navigate to={routes.HOME} />} />
+    </Routes>
   );
 };
