2f93c814ce3652c7b9adc492e4b42e5389c9f5bf
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 12:47:02 +0200

    Update Header.jsx

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,15 +1,16 @@
 import { Link, NavLink } from 'react-router-dom';
 import { Box } from 'components/Box/Box';
 import { HeaderBlock, Navigation } from './Header.styled';
+import { routes } from 'helpers/routes';
 
 export const Header = () => {
   return (
     <HeaderBlock>
       <Box maxWidth={1200} display="flex" justifyContent="space-between">
-        <Link to="/">Movies App</Link>
+        <Link to={routes.HOME}>Movies App</Link>
         <Navigation>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/movies">Movies</NavLink>
+          <NavLink to={routes.HOME}>Home</NavLink>
+          <NavLink to={routes.MOVIES}>Movies</NavLink>
         </Navigation>
       </Box>
     </HeaderBlock>
