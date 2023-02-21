d2dd3745a7895881a8972dcdcef186f77ea5a32f
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 17:32:33 +0200

    Update MovieDetails.jsx

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,32 +1,39 @@
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { getDetailedFilmInfo } from 'services/api';
+import { MovieInfo } from 'components/MovieInfo/MovieInfo';
 
-export const MovieDetailsPage = ({ id }) => {
-  const [detailedInfo, setDetailedInfo] = useState([]);
+export const MovieDetailsPage = () => {
+  const [detailedInfo, setDetailedInfo] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const { movieId } = useParams();
+
   useEffect(() => {
-    // if (!searchedQuery) return;
+    if (movieId === null) return;
+
     async function getDetailedInfo() {
       try {
         setIsLoading(true);
 
-        const { results } = await getDetailedFilmInfo(id);
-        setDetailedInfo(results);
+        const data = await getDetailedFilmInfo(movieId);
+        setDetailedInfo(data);
       } catch (error) {
         setError(error.message);
       } finally {
         setIsLoading(false);
       }
     }
+
     getDetailedInfo();
-  }, []);
+  }, [movieId]);
 
   return (
     <>
       {error && <div>Try to reload the page</div>}
       {isLoading && <div>Loading</div>}
+      {detailedInfo && <MovieInfo detailedInfo={detailedInfo} />}
     </>
   );
 };
