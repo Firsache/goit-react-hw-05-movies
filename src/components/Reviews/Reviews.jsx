b864edbf04e22c2c62899e620234250098f82fb9
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 17:32:42 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,3 +1,46 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getReviewsInfo } from 'services/api';
+
 export const Review = () => {
-  return <></>;
+  const [reviewInfo, setReviewInfo] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    if (movieId === null) return;
+
+    async function getApiReviewInfo() {
+      try {
+        setIsLoading(true);
+
+        const array = await getReviewsInfo(movieId);
+        console.log(array);
+
+        setReviewInfo(array);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getApiReviewInfo();
+  }, [movieId]);
+
+  return (
+    <>
+      {error && <div>Try to reload the page</div>}
+      {isLoading && <div>Loading</div>}
+      {reviewInfo && (
+        <p>
+          Review is here
+          {/* <p>Authot: {author}</p>
+          <p>{content}</p> */}
+        </p>
+      )}
+    </>
+  );
 };
