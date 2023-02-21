98b458647cab080265d482c9672b54f152747822
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 12:23:12 +0200

    Update Header.jsx

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,12 +1,17 @@
 import { Link, NavLink } from 'react-router-dom';
+import { Box } from 'components/Box/Box';
+import { HeaderBlock, Navigation } from './Header.styled';
+
 export const Header = () => {
   return (
-    <header>
-      <Link to="/">Movies App</Link>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/movies">Movies</NavLink>
-      </nav>
-    </header>
+    <HeaderBlock>
+      <Box maxWidth={1200} display="flex" justifyContent="space-between">
+        <Link to="/">Movies App</Link>
+        <Navigation>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+        </Navigation>
+      </Box>
+    </HeaderBlock>
   );
 };
