2574892d94a123173a6bd4785741facbe496dccd
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 12:57:05 +0200

    Update MovieDetails.jsx

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,3 +1,32 @@
-export const MovieDetailsPage = () => {
-  return <></>;
+import { useEffect, useState } from 'react';
+import { getDetailedFilmInfo } from 'services/api';
+
+export const MovieDetailsPage = ({ id }) => {
+  const [detailedInfo, setDetailedInfo] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    // if (!searchedQuery) return;
+    async function getDetailedInfo() {
+      try {
+        setIsLoading(true);
+
+        const { results } = await getDetailedFilmInfo(id);
+        setDetailedInfo(results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getDetailedInfo();
+  }, []);
+
+  return (
+    <>
+      {error && <div>Try to reload the page</div>}
+      {isLoading && <div>Loading</div>}
+    </>
+  );
 };
