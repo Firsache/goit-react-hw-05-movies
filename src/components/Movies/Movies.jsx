7358a174780f3025989e13a35e26da487b365fff
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 12:23:02 +0200

    Update Movies.jsx

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,3 +1,51 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getFilmByQuery } from 'services/api';
+import { Form } from 'components/Form/Form';
+
 export const MoviesPage = () => {
-  return <></>;
+  const [searchedMovies, setSearchedMovies] = useState([]);
+  const [searchedQuery, setSearchedQuery] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!searchedQuery) return;
+    async function getFilms() {
+      try {
+        setIsLoading(true);
+
+        const { results } = await getFilmByQuery(searchedQuery);
+        setSearchedMovies(results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getFilms();
+  }, [searchedQuery]);
+
+  const handleOnSubmit = value => {
+    setSearchedQuery(value);
+  };
+
+  return (
+    <>
+      {error && <div>Try to reload the page</div>}
+      {isLoading && <div>Loading</div>}
+      <Form onSubmit={handleOnSubmit} />
+      {searchedMovies && (
+        <ul>
+          {searchedMovies.map(({ id, original_title }) => (
+            <li key={id}>
+              <Link>
+                <h3>{original_title}</h3>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
