8f4889b14f4fe3f8b5e11db7f4124da9d1f1f551
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 17:32:47 +0200

    Update routes.js

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -2,6 +2,7 @@ export const routes = {
   HOME: '/',
   MOVIES: 'movies',
   MOVIE_DETAILS: 'movies/:movieId',
+  MOVIE_DETAILS_PATH: id => '/movies/' + id,
   CAST: 'cast',
   REVIEWS: 'reviews',
 };
