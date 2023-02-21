92c7b9149051977a233b2cc3a25a6fbeaba9de99
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 12:23:14 +0200

    Create Header.styled.js

---
+++ src/layout/Header/Header.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const HeaderBlock = styled.header`
+  margin: 0 auto;
+  padding: ${p => p.theme.space[5]}px ${p => p.theme.space[4]}px;
+  color: ${p => p.theme.colors.white};
+  background-color: ${p => p.theme.colors.backgroundColorHeader};
+
+  font-size: ${p => p.theme.fontSizes.m};
+`;
+
+export const Navigation = styled.nav`
+  display: flex;
+  gap: ${p => p.theme.space[5]}px;
+`;
