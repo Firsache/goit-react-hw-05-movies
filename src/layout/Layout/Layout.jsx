f9567bea169650ab3b616df3f39f9107d9060677
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-21 12:23:17 +0200

    Update Layout.jsx

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../styles/theme';
+import { GlobalStyles } from 'styles/GlobalStyles.styled';
 import { Header } from 'layout/Header/Header';
 
 export const Layout = () => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header />
       <main>{<Outlet />}</main>
-    </div>
+      <GlobalStyles />
+    </ThemeProvider>
   );
 };
